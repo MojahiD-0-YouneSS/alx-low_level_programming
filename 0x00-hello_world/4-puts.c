e3c02bc69ff2bf5ed4ac73054e6d3c6a78a17b7b
Youness Mojahid <mojahidyouness0@gmail.com>
2023-06-15 10:49:44 -0700

    hh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point !
- * Return: Allways 0 (yes)
+ *main - Entry point
+ *Return: Always 0 (yes)
  */
 
-
-
 int main(void)
-
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-}   	 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+} 	 
