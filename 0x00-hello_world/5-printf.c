65861fbcf0f0d524eeb8ebbb49df988d8fc1f5aa
Youness Mojahid <mojahidyouness0@gmail.com>
2023-06-15 10:58:48 -0700

    kk

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+  #include <stdio.h>
 
-/**
- * main - Entry point 
- * return : 0 as predected
- */
+ /**
+  * main - Entry point 
+  * return : 0 as predected
+  */
 
-int main(void)
-{
+  int main(void)
+  {
  
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+  }	
