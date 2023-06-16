a7957f1f6aafb7a5941d629235ca6c8aad695a51
Youness Mojahid <mojahidyouness0@gmail.com>
2023-06-16 10:54:09 -0700

    hello c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - our entry point of a c script
+ * Return: Always 1 value
+ */
+
+int main(void)
+{
+      write(2,"and that piece of art is useful\" - Dora korpar , 2015-10-19\n",59);
+      return (0);
+}
