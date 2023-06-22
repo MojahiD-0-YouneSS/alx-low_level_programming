6a6f938f0dd4dbad6a634b328e8e2eabdc813a32
Youness Mojahid <mojahidyouness0@gmail.com>
2023-06-22 15:48:38 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,15 @@
-#include "main"
+#include "main.h"
 
 
 
 
-int _isupper(int c) {
-    if (c >= 'A' && c <= 'Z') {
-        return 1;  // Character is uppercase
+int _isupper(int c)
+{
+    if ((c >= 'A') &&( c <= 'Z')) {
+        return 1; 
+    }else {
+    return 0;  
     }
-    return 0;  // Character is not uppercase
-}
+
+ }
 
