8c8d7db4715208d9565c70ea6fa37d39706cec6d
Youness Mojahid <mojahidyouness0@gmail.com>
2023-06-23 03:31:08 -0700

    hello

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void more_numbers(void) {
+    int i, j;
+
+    for (j = 0; j < 10; j++) {
+        for (i = 0; i <= 14; i++) {
+            if (i > 9) {
+                putchar((i / 10) + '0');
+            }
+            putchar((i % 10) + '0');
+        }
+
+        putchar('\n');
+    }
+}
