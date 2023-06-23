1419da5e13ac6e64a2a05bffcd9df73feda07b87
Youness Mojahid <mojahidyouness0@gmail.com>
2023-06-23 04:13:25 -0700

    jjj

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 
-
-void print_diagonal(int n)
-{
-    int a;
-    int b;
-    if ( n > 0)
-    {
-     for (a = 1; a <= n; a++)
-     {
-        for (b = 1; b < a; b++)
-	{
-	  _putchar(' ');
-	}
-	  _putchar('\\');
-	  _putchar('\n');
-	
-     }
-
-    }else
-    {
-     _putchar('\n');
+void print_diagonal(int n) {
+    int a, b;
+    
+    if (n > 0) {
+        for (a = 1; a <= n; a++) {
+            for (b = 1; b < a; b++) {
+                putchar(' ');
+            }
+            putchar('\\');
+            putchar('\n');
+        }
+    } else {
+        putchar('\n');
     }
 }
+
