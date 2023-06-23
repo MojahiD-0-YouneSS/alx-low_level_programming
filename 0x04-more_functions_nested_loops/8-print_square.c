d8b41b31221bfc2fd9502b2adc1447482de32932
Youness Mojahid <mojahidyouness0@gmail.com>
2023-06-23 04:16:32 -0700

    gg

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void print_square(int size) {
+    if (size <= 0) {
+        putchar('\n');
+        return;
+    }
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            putchar('#');
+        }
+        putchar('\n');
+    }
+}
