9d70f6285993977da9aa3b3813dcadc5c49bb416
Youness Mojahid <mojahidyouness0@gmail.com>
2023-06-26 11:36:39 -0700

    pointer to len and s

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+	while (*s != '\0')
+	{
+		len++;
+		++s
+	}
+
+	s--;
+
+	for (i = len; i > 0; i--)
+	{
+		putchar(*s);
+		s--;
+	}
+
+	putchar('\n');
+	
+}
