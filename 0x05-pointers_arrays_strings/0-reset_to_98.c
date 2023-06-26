38178e20b5bbf5285298dcec4748a9f2fcfbed36
Youness Mojahid <mojahidyouness0@gmail.com>
2023-06-26 10:48:50 -0700

    pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * rest_to_98 takes a pointer to an int as parameter
+ * and updates the values it ponts to to 98
+ * @n: pointer to int n
+ */
+
+void rest_to_98(int *n)
+{
+	*n = 98;
+}
