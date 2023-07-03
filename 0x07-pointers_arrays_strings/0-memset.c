4d4b78961502872125ff206922cb56d3a94400ee
Youness Mojahid <mojahidyouness0@gmail.com>
2023-07-03 16:13:25 -0700

    more pointers

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * _memset - fill n bytes of memory with a constant value
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	char *start = s;
+
+	while (i < n)
+	{
+		*s++ = b;
+		i++;
+	}
+	return (start);
+}
