495f4bc9d1c4b99f543f38bf34b613ae82c8dcf9
Youness Mojahid <mojahidyouness0@gmail.com>
2023-07-03 18:07:54 -0700

    joy

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int consistC = 0;
+	int prevC;
+
+	while (*s)
+	{
+		i = 0;
+		prevC = consistC;
+		while (*(accept + i) != '\0')
+		{
+			if (*(accept + i) == *s)
+				consistC++;
+			i++;
+		}
+		if (prevC == consistC) /* didn't equal a char from accept */
+			break;
+		s++;
+	}
+	return (consistC);
+}
