e50fe1d93cdba7ac1356ec9aaa0475420f0abc3e
Youness Mojahid <mojahidyouness0@gmail.com>
2023-07-03 18:28:42 -0700

    new

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-void set_string(char **s, char *to);
-{
-	*s = to;
+void set_string(char **s, char *to) {
+    // Free the memory if the pointer is already pointing to a string
+    if (*s != NULL) {
+        free(*s);
+    }
+
+    // Allocate memory for the new string
+    *s = (char*) malloc((strlen(to) + 1) * sizeof(char));
+
+    // Copy the contents of 'to' into the newly allocated memory
+    strcpy(*s, to);
 }
