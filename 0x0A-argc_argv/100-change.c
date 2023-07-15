e004f26559ef0c48f1a0a8a1ad183352ef574eed
Youness Mojahid <mojahidyouness0@gmail.com>
2023-07-15 09:57:03 -0700

    joy

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,44 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
-/**
- * main - prints the minimum number of coins for an amount of money
- * @argc: should count two arguments
- * @argv: arguments given should be program name and amount of money
- * Return: least number of coins, 0 if negative amount, 1 if amount not given
- */
+int minimumCoins(int cents) {
+    if (cents < 0) {
+        return 0;
+    }
 
-int main(int argc, char *argv[])
-{
-	int n, coins = 0;
+    int coinValues[] = {25, 10, 5, 2, 1};
+    int numCoins = sizeof(coinValues) / sizeof(coinValues[0]);
+    int count = 0;
 
-	/* validate input */
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+    for (int i = 0; i < numCoins; i++) {
+        count += cents / coinValues[i];
+        cents %= coinValues[i];
+    }
 
-	if (argv[1][0] == '-')
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	/* convert string to int and calculate coins */
-	n = atoi(argv[1]);
-
-	coins += n / 25;
-	n = n % 25;
-	coins += n / 10;
-	n = n % 10;
-	coins += n / 5;
-	n = n % 5;
-	coins += n / 2;
-	n = n % 2;
-	coins += n / 1;
-
-	printf("%d\n", coins);
-	return (0);
+    return count;
 }
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Error\n");
+        return 1;
+    }
+
+    int cents = atoi(argv[1]);
+
+    int minCoins = minimumCoins(cents);
+    printf("%d\n", minCoins);
+
+    return 0;
+}
+
