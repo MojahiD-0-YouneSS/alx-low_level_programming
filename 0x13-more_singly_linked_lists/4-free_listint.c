4f9bf8ac6bea54a3af566eb885fbea264617aedd
Youness Mojahid <mojahidyouness0@gmail.com>
2023-08-01 13:27:43 -0700

    hello.c#2

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,22 +2,22 @@
 #include <stdlib.h>
 
 /**
- * free_listint2 - frees a listint list
+ * free_listint - frees a listint list
  *
  * @head: head of list to free
  *
  * Return: void
  */
-void free_listint2(listint_t **head)
+void free_listint(listint_t *head)
 {
 	listint_t *next;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return;
 
-	for (next = (*head)->next; *head != NULL; *head = next)
+	for (next = head->next; head != NULL; head = next)
 	{
-		next = (*head)->next;
-		free(*head);
+		next = head->next;
+		free(head);
 	}
 }
