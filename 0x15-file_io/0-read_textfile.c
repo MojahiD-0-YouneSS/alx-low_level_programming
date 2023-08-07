cc72743ea4eb13bda8ec7436368e930862b8e8c8
Youness Mojahid <mojahidyouness0@gmail.com>
2023-08-07 15:45:41 -0700

    hqllo

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "tail.h"
+#include <stdlib.h>
+
+/*
+ *
+ * a file I/O in c is refers to Input/Output operations involving files in the C programming language. It allows you to read data from files (input) and write data to files (output). The standard library provides functions to perform these operations.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+       ssize_t D, r, Q;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	D = open(filename, O_RDONLY);
+	r = read(D, buffer, letters);
+	Q = write(STDOUT_FILENO, buffer, r);
+
+	if (D == -1 || r == -1 || Q == -1 || Q != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(D);
+
+	return (Q);
+}
