a4f33a1b1a9a13c8a4422e118f2b2a17a0b5d40c
Mojahid Youness <mojahidyouness0@gmail.com>
2023-10-25 14:12:56 -0700

    return2

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,11 +15,11 @@ int _pow_recursion(int x, int y)
 	}
 	if (x == 1 || y == 0)
 	{
-		return(0);
+		return(1);
 	}
 	if (x == 0)
 	{
-		return(1);
+		return(0);
 	}
 	return (x * _pow_recursion(x,y-1));
         
