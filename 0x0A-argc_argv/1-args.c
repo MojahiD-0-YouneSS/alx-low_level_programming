9875a96a9ef76c3eb8644a1d63c0a91aa9abe5f9
Mojahid Youness <mojahidyouness0@gmail.com>
2023-10-29 05:12:39 -0700

    power3

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,15 +5,14 @@
  * main - print each argument passed to program on a line
  * @argc: argument counter
  * @argv: pointer to array of arguments
- * Return: 0 on success
+ * Return: 0 if successed
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv)
 {
-	int i = 0;
-
-	while (i < argc)
-		printf("%s\n", argv[i++]);
-
+        if (argc > 0)
+	{
+		printf("%d\n",argc -1);
+	}
 	return (0);
 }
