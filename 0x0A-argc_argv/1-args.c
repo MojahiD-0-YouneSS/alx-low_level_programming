01cdb307d06dff0039590be04e8f4aba932144c9
Mojahid Youness <mojahidyouness0@gmail.com>
2023-10-29 05:19:11 -0700

    power4

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - print each argument passed to program on a line
@@ -10,9 +9,6 @@
 
 int main(int argc, char *argv)
 {
-        if (argc > 0)
-	{
-		printf("%d\n",argc -1);
-	}
-	return (0);
+    printf("%d\n",argc -1);	
+    return (0);
 }
