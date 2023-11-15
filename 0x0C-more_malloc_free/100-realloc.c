aafd1345bd6b755fb3591d29d409149993dd32aa
Mojahid Youness <mojahidyouness0@gmail.com>
2023-11-15 05:31:41 -0800

    mre free fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _realloc - a function that reallocates a memory block using malloc and free
+ * * _realloc - a function that reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previsouly allocated by malloc
  * @old_size: size of the allocated memory for ptr
  * @new_size: new size of the new memory block
@@ -33,7 +33,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
         }
 
 	pts1 = malloc(new_size);
-	
 	if (!pts1)
 	{
 		return (NULL);
