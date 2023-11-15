107d1f9a6dcbecdc236305c81ef9552ccc9c510d
Mojahid Youness <mojahidyouness0@gmail.com>
2023-11-15 01:00:00 -0800

    dog fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * init_dog -  a function that initializes a variable of type struct dog
+ * init_dog - a function that initializes a variable of type struct dog
  * @d: pointer to struct dog to initialize
  * @name: name to initialize
  * @age: age to initialize
