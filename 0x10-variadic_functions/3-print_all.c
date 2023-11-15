6f03a0b5dff37f65788766bfde7ab4aada8a0adc
Mojahid Youness <mojahidyouness0@gmail.com>
2023-11-15 01:35:45 -0800

    variadic fixes asap

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,6 +8,7 @@
 void print_all(const char * const format, ...)
 {
 	int i = 0;
+
 	char *str, *sep = "";
 
 	va_list list;
@@ -23,32 +24,25 @@ void print_all(const char * const format, ...)
 				case 'c':
 					printf("%s%c", sep, va_arg(list, int));
 					break;
-
 				case 'i':
 					printf("%s%d", sep, va_arg(list, int));
 					break;
-
 				case 'f':
 					printf("%s%f", sep, va_arg(list, double));
 					break;
-
 				case 's':
 					str = va_arg(list, char *);
 
 					if (!str)
 						str = "(nil)";
-
+					
 					printf("%s%s", sep, str);
-
 					break;
-
 				default:
 					i++;
 					continue;
 			}
-
 			sep = ", ";
-
 			i++;
 		}
 	}
