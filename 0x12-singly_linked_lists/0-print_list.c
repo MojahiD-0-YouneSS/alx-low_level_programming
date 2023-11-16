e34833748c23d7e4a0ffb64d3870adeaca0c3dc5
Mojahid Youness <mojahidyouness0@gmail.com>
2023-11-16 13:51:12 -0800

    *4

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,18 +8,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t a = 0;
+	size_t len = 0;
 
 	if (h == NULL)
 		return (0);
 	while (h != NULL)
 	{
 		if (h->str != NULL)
-			printf("[%d] %s\n", h->a, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
-		a++;
+	len++;
 		h = h->next;
 	}
-	return (a);
+	return (len);
 }
